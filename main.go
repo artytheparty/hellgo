c945fab05d38c7f19d8a88155db1a701212ce223
Mehrab Rahman <mehrab.rahman@revature.com>
2019-10-02 11:45:49 -0500

    Refactored into functions

--- main.go
+++ main.go
@@ -6,23 +6,30 @@ import (
 )
 
 func main() {
-	var locale string
-	var translations = make(map[string]string)
-	translations["en"] = "Hello"
-	translations["de"] = "Guten tag"
-	translations["fr"] = "Bonjour"
-
-	if len(os.Args) == 1 {
-		fmt.Println("Enter language code:")
-		fmt.Scanf("%s", &locale)
-	} else {
-		locale = os.Args[1]
-	}
-
-	output := translations[locale]
+	locale := argswitch(os.Args)
+	output := translate(locale)
 	if output == "" {
 		output = "Yo"
 	}
 
 	fmt.Println(output, "Go!")
+}
+
+func translate(input string) string {
+	var translations = make(map[string]string)
+	translations["en"] = "Hello"
+	translations["de"] = "Guten tag"
+	translations["fr"] = "Bonjour"
+	return translations[input]
+}
+
+func argswitch(input []string) string {
+	var locale string
+	if len(input) == 1 {
+		fmt.Println("Enter language code:")
+		fmt.Scanf("%s", &locale)
+	} else {
+		locale = input[1]
+	}
+	return locale
 }
